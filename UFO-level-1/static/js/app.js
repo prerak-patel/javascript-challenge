b6d8ab3e534abe652b3529da753b87625df91183
prerak-patel <emailprerak@gmail.com>
2020-05-11 22:31:17 -0500

    Level 1: Automatic Table and Date Search

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+var filterbutton = d3.select("#filter-btn");
+var datetime = d3.select("#datetime")
 
-// YOUR CODE HERE!
+data.forEach((tableData) => {
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// Filter records by date
+filterbutton.on("click", function() {
+  // Clear table contents before appending filtered results
+  $("#ufo-table").find("tbody").empty();
+  
+  inputValue = datetime.property("value");
+  var filteredData = tableData.filter(data => data.datetime === inputValue);
+  console.log(filteredData);
+
+
+  filteredData.forEach((filteredData) => {
+    var row = tbody.append("tr");
+    Object.entries(filteredData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+});
+
+
+
